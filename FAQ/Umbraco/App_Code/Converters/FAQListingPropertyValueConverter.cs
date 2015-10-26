0fa68eec8b2028384dce70134be447b1f7dc615e
Kyle Weems <kyle@mindfly.com>
2015-10-26 11:33:26 -0700

    Property converter and models to easily use FAQListing in Razor.

---
+++ FAQ/Umbraco/App_Code/Converters/FAQListingPropertyValueConverter.cs
@@ -0,0 +1,26 @@
+﻿
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+public class FAQListingPropertyValueConverter : IPropertyValueConverter
+{
+    public bool IsConverter(PublishedPropertyType propertyType)
+    {
+        return propertyType.PropertyEditorAlias == "faq.listing.editor";
+    }
+
+    public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+    {
+        return source;
+    }
+
+    public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+    {
+        return FAQListing.Deserialize(source as string);
+    }
+
+    public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+    {
+        return null;
+    }
+}
---
+++ FAQ/Umbraco/App_Code/Models/FAQItem.cs
@@ -0,0 +1,41 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+/// <summary>
+/// FAQItem is a FAQ Item.
+/// </summary>
+public class FAQItem
+{
+    [JsonProperty("question")]
+    public string Question { get; set; }
+
+    [JsonProperty("answer")]
+    public IHtmlString Answer { get; set; }
+
+    [JsonProperty("categories")]
+    public IEnumerable<string> Categories { get; set; }
+
+    public static FAQItem Deserialize(string json)
+    {
+        // Validate the JSON
+        if (json == null || !json.StartsWith("{") || !json.EndsWith("}"))
+        {
+            return null;
+        }
+
+        // Deserialize the JSON
+        var jobj = (JProperty)JsonConvert.DeserializeObject(json);
+        return new FAQItem()
+                   {
+                       Question = (string)jobj.Value["question"],
+                       Answer = MvcHtmlString.Create(jobj.Value<string>("answer")),
+                       Categories = jobj.Value["categories"].ToString().Split(',').ToList()
+                   };
+    }
+}
